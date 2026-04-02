5a5464a01f7e3d2b4418d4d0780e7ee1b59cf560
Carol Savchick <carolmariehinds@gmail.com>
2026-04-01 20:33:22 -0400

    begin index html

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ const express = require("express");
 const app = express();
 
 const port = 8080;
+
+
+app.use(express.static("./public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// 11&12 for POST requests
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+    console.log("Press Ctrl+C to end this process.");
+});
+
+
+//static array for the creative limitations--user does not select, the data gets transfered over to the self-eval form API
+//time start, time end, self-eval score, creative limitation, 
+//API route for the forms--the self-eval form & creative limitation form
+//ACTION ITEM: probide Dan w/ the full shape of what I'm storing--what info I am storing. Get it to him ASAP!
+//reference Dan's repo--"full CRUD api" branches
